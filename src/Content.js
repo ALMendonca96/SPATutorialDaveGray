50d57d93eda6263263cac2d10761836fd38a9e26
Anderson Mendonça <andersonluis161@gmail.com>
2023-06-01 10:21:48 -0300

    add click events

--- src/Content.js
+++ src/Content.js
@@ -2,14 +2,29 @@ import React from "react";
 
 const Content = () => {
   const handleNameChange = () => {
-    const names = ["Bob", "Kevin", "Anderson"];
+    const names = ["Obi-Wan", "Anakin", "Asoka"];
     const randomInt = Math.floor(Math.random() * 3);
     return names[randomInt];
   };
 
+  const handleClick = () => {
+    console.log("You clicked it");
+  };
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
   return (
     <main>
-      <p>Hello {handleNameChange()}</p>
+      <p onDoubleClick={handleClick}>Hello {handleNameChange()}</p>
+      <button onClick={handleClick}>Click It</button>
+      <button onClick={() => handleClick2("Anderson")}>Click It</button>
+      <button onClick={(e) => handleClick3(e)}>Click It</button>
     </main>
   );
 };
