a6396d626d261c13e3473ffc0f4306c36af10f10
Anderson Mendonça <andersonluis161@gmail.com>
2023-06-01 20:22:48 -0300

    add props and prop drilling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import { Header } from "./Header";
+import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([
+    { id: 1, checked: false, item: "Lightsaber" },
+    { id: 2, checked: false, item: "Stormtrooper Helmet" },
+    { id: 3, checked: false, item: "Obi-Wan's Robe" },
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+  };
+
   return (
     <div className="App">
-      <Header />
-      <Content />
-      <Footer />
+      <Header title="Vader's Pocket" />
+      <Content
+        items={items}
+        setItems={setItems}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-export const Header = () => {
+const Header = ({ title }) => {
   return (
     <header>
-      <h1>Groceries List</h1>
+      <h1>{title}</h1>
     </header>
   );
 };
+
+Header.defaultProps = {
+  title: "Default Title",
+};
+
+export default Header;
